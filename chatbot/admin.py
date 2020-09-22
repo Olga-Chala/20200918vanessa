f3ba4bb10d9f4a6f7891262ec2df7136b4d8f6dc
20200918Vanessa <olgachalas@gmail.com>
2020-09-22 11:47:48 +0300

    Modified DB, second commit

--- chatbot/admin.py
+++ chatbot/admin.py
@@ -2,5 +2,17 @@ from django.contrib import admin
 from .models import User_Answers
 from .models import ABC_Modules
 
-admin.site.register(User_Answers)
-admin.site.register(ABC_Modules)
+class ABC_Modules_Admin(admin.ModelAdmin):
+    list_display = ('code_abc_modules','created_date', 'module_type', 'module_level', 'module_used', 'vanessa_question','url_address')
+    search_fields = ('vanessa_question',)
+    list_display_links = ('module_type', 'module_level', 'vanessa_question','url_address')
+
+
+class User_Answers_Admin(admin.ModelAdmin):
+    list_display = ('module_guestion_field', 'created_date', 'keyword','keyword_weight')
+    search_fields = ('keyword','keyword_weight')
+    # list_display_links = ('created_date', 'keyword','keyword_weight')
+
+
+admin.site.register(User_Answers, User_Answers_Admin)
+admin.site.register(ABC_Modules, ABC_Modules_Admin)
--- chatbot/models.py
+++ chatbot/models.py
@@ -3,31 +3,53 @@ from django.db import models
 from django.utils import timezone
 import uuid
 
-class User_Answers(models.Model):    
-    code_keys = models.UUIDField(primary_key=True, unique=True, editable=False, null=False, default=uuid.uuid4)
-    title_block = models.CharField(max_length=200)
-    keyword = models.CharField(max_length=200)
-    module_code = models.CharField(max_length=200)
-    weight = models.CharField(max_length=200)
-    
-    def publish_keys(self):
-        self.save()
-
-    def __str__(self):
-        return self.title_block
-
 class ABC_Modules(models.Model):
+    verbose_name = "Модуль вопросов и ответов Ванессы"
+    verbose_name_plural = "Модули вопросов и ответов Ванессы"  
     code_abc_modules = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    # code_keys = models.ForeignKey(User_Answers, on_delete=models.CASCADE,default=0)
-    module_type = models.CharField(max_length=2)
-    web_interface = models.TextField(default='google.com')
-    vanessa_answ = models.TextField()
-    created_date = models.DateTimeField(default=timezone.now)
-    next_module_code = models.CharField(max_length=200, default='')
+    created_date = models.DateTimeField('Дата создания модуля', default=timezone.now)
+    module_type = models.CharField('Тип модуля', max_length=1)
+    module_level = models.IntegerField('Уровень модуля', default=0)
+    module_used = models.BooleanField('Модуль используется Ванессой', default=True)
+    previous_module = models.ForeignKey('self', on_delete=models.PROTECT, blank=True, null=True)
+    # ,  related_name='Предшествующий модуль'
+    # previous_module = models.IntegerField('Предшествующий модуль', null=True)
+    vanessa_question = models.CharField('Вопрос Ванессы', max_length=200, default='')
+    Vanessa_answer_word = models.BooleanField('Использовать слова пользователя в ответе', default=False)
+    vanessa_answer = models.CharField('Ответ Ванессы', max_length=200, blank=True)
+    web_interface = models.CharField('Интерфейс', max_length=200, default='Интерфейс', blank=True)
+    url_address = models.URLField('url', max_length=200, blank=True, null=True)
 
-    def publish(self):
+    def ABC_Modules_save(self):
+        self.created_date = timezone.now()
+        self.save()
+        
+    # def __str__():
+    #     self.vanessa_question
+
+
+class User_Answers(models.Model):  
+    verbose_name = "Ключевое слово в ответе пользователя"
+    verbose_name_plural = "Ключевые слова в ответе пользователя"
+    code_keys = models.UUIDField(primary_key=True, unique=True, editable=False, default=0)
+    created_date = models.DateTimeField('Дата', default=timezone.now)
+    module_code = models.ForeignKey('ABC_Modules', on_delete=models.PROTECT, default=1)
+    keyword = models.CharField(' Ключевое слово', max_length=30)
+    keyword_weight = models.FloatField('Вес(важность) слова', blank=True, null=True)
+
+    def User_Answers_save(self):
         self.created_date = timezone.now()
         self.save()
 
+    def module_guestion_field(self):
+        verbose_name = "Модуль вопросов и ответов Ванессы"
+        return ABC_Modules.vanessa_question
+        
+    def module_type_field(self):
+        return ABC_Modules.module_type
+
+    def module_level_field(self):
+        return ABC_Modules.module_level
+    
     def __str__(self):
-        return self.vanessa_answ
+        return ABC_Modules.vanessa_question
