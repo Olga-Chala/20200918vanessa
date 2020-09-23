b9d4138b0cb9bdfe7549b99ce5556bc43a5627ed
20200918Vanessa <olgachalas@gmail.com>
2020-09-23 17:40:49 +0300

    new admin interf

--- chatbot/views.py
+++ chatbot/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import VanessaModule
 
 def abc_modules_list(request):
     return HttpResponse("...to be continued")
+
+# def find_row(request):
+#     # if VanessaModule.objects.all().filter(module_number=0):
+#     #     return HttpResponse(VanessaModule.vanessa_question)
+#     # b = VanessaModule.objects.filter(module_number=0)
+#     b = VanessaModule.objects
+#     return HttpResponse(b.vanessa_question)
+
